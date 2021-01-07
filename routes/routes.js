eb1698be667ac5c011b37ebf62453106cfea0b56
rastangineer <kike.cuevas@gmail.com>
2021-01-06 23:16:14 -0600

    add initial version of fibonacci generation, needs optimization

--- routes/routes.js
+++ routes/routes.js
@@ -2,12 +2,32 @@ var appRouter = function(app) {
 	app.get("/", function(req, res) {
 		if (!req.query.number) {
 			return res.send({"status": "error", "message": "missing number"});
-		} else {
-			return res.send ("Number provided is " + req.query.number);
+		} 
+		if (req.query.number<0) {
+			return res.send({"status": "error", "message": "number should be positive"});
+		}
+		else {
+			result = fibonacci(req.query.number);
+			return res.send ({"status": "success", "result": result});
+			
 		}
 	});
 
 	
+}
+
+function fibonacci(num){
+	//console.log("FIB: num is ",num);
+	if (num == 0) {
+		return 0;
+	}
+	else if (num == 1) {
+		return 1;
+	}
+	else {
+		console.log(num-2,"+" ,num-1);
+		return Number(fibonacci (num-2)) + Number(fibonacci (num-1));
+	}
 }
 
 module.exports = appRouter;
